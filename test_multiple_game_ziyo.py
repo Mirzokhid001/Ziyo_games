e61fda5cf5988cae7b34a225831ec12f0a42440a
Mirzokhid001 <71182412+Mirzokhid001@users.noreply.github.com>
2020-12-02 04:18:04 +0100

    Create test_multiple_game_ziyo.py

---
+++ test_multiple_game_ziyo.py
@@ -0,0 +1,8 @@
+from multiple_game_ziyo import multiplying
+import pytest
+def test_multiplying[]:
+    test1 = multiplying(2, 5)
+    assert(test1 == 10)
+
+    test2 = multiplying(3, 3)
+    assert(test2 == 6)
